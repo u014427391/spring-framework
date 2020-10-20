ecb8dc6ebfcd5670641c530cd5c26ad214edab56
nicky ma <mazq@minstone.com.cn>
2020-10-20 13:42:47 +0800

    add spring example project

---
+++ spring-example/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    java
+}
+
+group = "org.springframework"
+version = "5.0.20.BUILD-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
---
+++ spring-example/src/main/java/org/springframework/example.java
@@ -0,0 +1,19 @@
+package org.springframework;
+
+/**
+ * <pre>
+ *      Spring测试类
+ * </pre>
+ *
+ * <pre>
+ * @author mazq
+ * 修改记录
+ *    修改后版本:     修改人：  修改日期: 2020/10/20 12:03  修改内容:
+ * </pre>
+ */
+public class example {
+
+    public static void main(String[] args) {
+        System.out.println("spring");
+    }
+}
